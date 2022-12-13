de774dadabfa9a7c389ec6ede61110f9728126a2
Thong LT <letrthong@gmail.com>
2022-12-13 22:19:22 +0700

    g
    .

---
+++ hello_world/src/main.c
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2012-2014 Wind River Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/kernel.h>
+
+#include <zephyr/drivers/gpio.h>
+
+void main(void)
+{
+	
+	
+	("Hello World! %s\n", CONFIG_BOARD);
+}
